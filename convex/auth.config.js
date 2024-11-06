9a04ff7f3a945335739f63dac7991da0f57075bf
sushmitha2410 <sush70327lv@gmail.com>
2024-11-06 14:03:01 +0530

    fifth commit

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers:[
     {
-      domain:'https://beloved-leech-8.clerk.accounts.dev',
+      domain:'https://fancy-hermit-15.clerk.accounts.dev',
       applicationID:"convex", 
     }
   ]
